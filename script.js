1b30806ce348b12d6297a47d9966bd82fd1c7a5a
asaulters <71015650+asaulters@users.noreply.github.com>
2021-04-13 21:11:20 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -11,13 +11,38 @@ function pullQuote(){
 
 
 
-let quoteAPI =  'https://programming-quotes-api.herokuapp.com/';
-// let quoteRequest = 
+
+document.addEventListener('DOMContentLoaded', () => {
+    //DOM Elements
+    const quoteDiv = document.querySelector('.main-TopQuote');
+
+    async function updateQuote(){
+        //fetch a random qyote from API
+        const response = await
+        fetch("https://api.quotable.io/random");
+        const data = await response.json();
+        if (response.ok) {
+          // Update DOM elements
+          quote.textContent = data.content;
+          cite.textContent = data.author;
+        } else {
+          quote.textContent = "An error occured";
+          console.log(data);
+    }
+
+}
+
+  // Attach an event listener to the `button`
+  button.addEventListener("click", updateQuote);
+
+  // call updateQuote once when page loads
+  updateQuote();
+});
 
 
-fetch('https://programming-quotes-api.herokuapp.com/')
-    .then(response => {
-        console.log(response);
-        
+
+fetch('https://api.quotable.io/random')
+    .then(response => response.json())
+    .then(data => {
+    console.log(`${data.content} —${data.author}`)
     })
-    .then(data => console.log(data))
